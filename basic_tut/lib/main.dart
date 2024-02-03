15325469025064f125b3bcde5ded5af684f11b7b
HARSH TRIPATHI <mynameisharsh143@gmail.com>
2024-02-04 01:20:27 +0530

    Day3 completed

--- basic_tut/lib/main.dart
+++ basic_tut/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:basic_tut/home_page.dart';
+import 'package:basic_tut/pages/home_page.dart';
+import 'package:basic_tut/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,14 +7,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
+  @override // Myapp and statelessWidget both are using  build to do their own task thus overiding the same method build
   Widget build(BuildContext context) {
-   
-
-    
+    //Widget is a component
+    //context help tell us the position of a different things in flutter
 
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,//first declare theme
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,// adjust all types of widget according to given color
+      ),
+      darkTheme: ThemeData( //then intilize how do you want your theme to be and return to themeMode
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home", //to use which route at the start of the app
+      routes: {
+        "/": (context) =>  LoginPage(), //does the same work as home: Homepage(), this is object of class HomePage(), can also use new 
+        "/home": (context) =>  HomePage(),
+        "/login": (context) =>  LoginPage(),
+      }, //tells direction
     );
   }
 }
--- basic_tut/lib/pages/home_page.dart
+++ basic_tut/lib/pages/home_page.dart
@@ -4,7 +4,8 @@ class HomePage extends StatelessWidget {
    final int days = 70;
     final String name = "Harsh";
   const HomePage({super.key});
-
+// we can do arithematic operation in final but not in const 
+   //method is a function which does a work which either return or does not return 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,5 +20,10 @@ class HomePage extends StatelessWidget {
         ),
         drawer: Drawer(),
       );
+  }
+// @required is used to make something mandatory 
+// to use a bool you must declared whether it is true or not
+  bringVegetables(/*here comes a value to pass*/){ //this is a method  we can use optional parameter using curly bracket
+
   }
 }
